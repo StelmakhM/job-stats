456709a1bfe930ff3053e42c7be6cc60cca922e6
Maksym Stelmakh <stelmakh1994@gmail.com>
2023-01-15 15:59:02 +0200

    add start page

---
+++ src/components/Logo.jsx
@@ -0,0 +1,5 @@
+import logo from "../images/logo.svg";
+
+export default function Logo() {
+	return <img src={logo} alt="jobster logo" className="logo" />;
+}
---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,28 @@
+import Wrapper from "./Landing.styled";
+import main from "../../images/main.svg";
+import { Logo } from "../../components";
+
+const Landing = () => {
+	return (
+		<Wrapper>
+			<nav>
+				<Logo />
+			</nav>
+			<div className="container page">
+				<div className="info">
+					<h1>
+						job <span>tracking</span> app
+					</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse perferendis rerum necessitatibus consequuntur voluptatem adipisci repudiandae praesentium quaerat deleniti
+						dignissimos!
+					</p>
+					<button className="btn btn-hero">Login/Register</button>
+				</div>
+				<img src={main} alt="job hunt" className="img main-img" />
+			</div>
+		</Wrapper>
+	);
+};
+
+export default Landing;
